43595183c9e248d009759e2f5121777f9ebf5933
Sakshi Singh <sakshisingh291999@gmail.com>
2022-02-12 16:56:14 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React ,{useEffect,useState} from 'react'
+import { BrowserRouter } from 'react-router-dom';
+import "./App.css";
+import Homepage from './Components/Homepage/Homepage';
+
+function App() {
+  return (
+     
+    <BrowserRouter>  
+       <Homepage/>
+    </BrowserRouter>
+  );
+}
+
+export default App
---
+++ src/Components/Homepage/Homepage.js
@@ -0,0 +1,65 @@
+import React , {useEffect,useState} from 'react'
+import './home.css';
+import { Link } from "react-router-dom";
+
+function Homepage() {
+
+
+    const [data, setData] = useState([]);
+
+    const getData = async ()=>{
+       const res= await fetch('https://jsonplaceholder.typicode.com/users');
+       const actualData= await (res.json());
+       console.log(actualData);
+       setData(actualData);    
+    }
+   
+
+    useEffect(() => {
+      getData();
+    }, [])
+    
+
+ return (
+        
+    <>
+   
+            <div className="">
+                 
+            </div>
+            <div className="container-fluid ">
+                    <div className="table-responsive">
+                        <div >
+                            <div className="thead-dark ">
+                                <div className="row">
+                                    <div className="colsize">NAME</div>
+                                    <div className="colsize">COMPANY</div>
+                                    <div className="colsize">BLOG POSTS</div>
+                                </div>
+                            </div>
+                            <div>
+                            {
+                                data.map((currelement,index) => {
+                                return (
+                                    <div className="row" key={index}>
+                                        <div className="colsize">{currelement.name}</div>
+                                        <div className="colsize">{currelement.company.name}</div>
+                                        <div className="colsize">
+                                        <Link to={`/posts/${currelement.id}`}>Posts</Link>
+                                        </div>
+                                    </div>
+                                )
+                                }
+                                )
+                            }
+                            </div>
+                        </div>
+                    </div>
+        </div>
+    </>  
+
+
+  );
+}
+
+export default Homepage
